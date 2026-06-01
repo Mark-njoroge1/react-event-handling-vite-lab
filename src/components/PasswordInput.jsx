82e30966ac114d174ea0540ca4bd625f35688023
Mark Njoroge <marknjoroge591@gmail.com>
2026-06-01 12:26:51 +0300

    Completed password setup and button

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,18 @@
-// Code PasswordInput Component Here
+import React, {useState} from 'react';
 
 function PasswordInput (){
+    const [password, setPassword] = useState('');
+    const handleInputChange = (e) => {
+        setPassword(e.target.value);
+        console.log("User is typing...");
+    };
     return(
-        <></>
-    )
+        <input
+           type="password"
+           value={password}
+           onChange={handleInputChange}
+           placeholder="Enter your password"
+        />
+    );
 }
-
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,23 @@
-// Code SubmitButton Component Here
+import React from 'react';
+
 
 function SubmitButton (){
+    const handleMouseEnter = () => {
+        console.log("Mouse entered the button area.");
+    };
+    const handleMouseLeave = () => {
+        console.log("Mouse left the button area.");
+    };
     return(
-        <></>
-    )
+        <button
+        type="button"
+        onMouseEnter={handleMoueEnter}
+        onMouseLeave={handleMouseLeave}
+        >
+            submit password{/* Text must exactly match*/}
+        </button>
+        
+    );
 }
 
 export default SubmitButton;
